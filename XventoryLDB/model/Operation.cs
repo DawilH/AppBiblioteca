b7cb7c2c4127c387f16e8d769225eecb95b2ee5a
DawilH <dawilherrerareyes@gmail.com>
2022-08-04 18:44:33 -0400

    Agregar archivos de proyecto.

---
+++ XventoryLDB/model/Operation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XventoryLDB
+{
+    class Operation
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Q { get; set; }
+        public int Kind { get; set; } 
+    }
+}
---
+++ XventoryLDB/model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XventoryLDB
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PriceIn { get; set; }
+        public string PriceOut { get; set; }
+
+        public DateTime fecha = DateTime.Now;
+
+
+    }
+}
